f5bf877088073f9ed49596eae165e4a25f1517da
Alex Vanderpot <alex@vanderpot.com>
2023-08-03 19:22:36 -0700

    hello

--- src/app/api/hello/route.tsx
+++ src/app/api/hello/route.tsx
@@ -1,9 +1,27 @@
 import { NextResponse } from 'next/server'
+import {Request} from "next/dist/compiled/@edge-runtime/primitives";
 
 export const runtime = 'edge'
 
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
+export async function OPTIONS(request: Request){
+    return new NextResponse('', {
+        status: 200,
+        headers: {
+            ...corsHeaders
+        },
+    })
+}
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
 
-    return NextResponse.json({ searchParams })
+    const res = NextResponse.json({ searchParams })
+    Object.entries(corsHeaders).forEach(([k,v])=>res.headers.set(k,v))
+
+    return res
 }
